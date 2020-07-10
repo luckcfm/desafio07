32cc691599040eb6d1a8a52cb14129a81c60fdd7
Lucas Medeiros <lucasmedeiros@viacast.tv>
2020-07-10 01:57:50 -0300

    removendo codigo n utilizado.

--- src/pages/Import/index.tsx
+++ src/pages/Import/index.tsx
@@ -1,8 +1,6 @@
 import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 
-import filesize from 'filesize';
-
 import Header from '../../components/Header';
 import FileList from '../../components/FileList';
 import Upload from '../../components/Upload';
